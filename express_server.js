12cd0195e5446aef0a17e19fe852d094160cac1a
Kathy <kathy.tam@mail.mcgill.ca>
2021-07-06 01:09:46 +0000

    Add redirect after form submission

--- express_server.js
+++ express_server.js
@@ -45,8 +45,10 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  // Store longURL-shortURL pair in database
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
