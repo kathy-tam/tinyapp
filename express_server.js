d7dee3b2dc6b994293683bab03470c54fcedbc7a
Kathy <kathy.tam@mail.mcgill.ca>
2021-07-07 21:55:19 +0000

    Create user registration form

--- express_server.js
+++ express_server.js
@@ -87,6 +87,12 @@ app.post("/logout", (req, res) => {
   res.redirect('/urls/');
 });
 
+// Registration page
+app.get('/register', (req, res) => {
+  const templateVars = { username: req.cookies["username"] };
+  res.render('registration.ejs', templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
